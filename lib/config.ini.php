faca91da288985fa5936c7b899ca5456dae702f2
Abderrahmane <abdoel132003@gmail.com>
2025-04-08 21:16:04 +0100

    initial commit

---
+++ lib/config.ini.php
@@ -0,0 +1,31 @@
+<?php 
+	/** 
+	* Configuration
+
+	* @package Wojo Framework
+	* @author wojoscripts.com
+	* @copyright 2025
+	* @version Id: config.ini.php, v1.00 2025-04-07 10:45:07 gewa Exp $
+	*/
+ 
+	 if (!defined("_WOJO")) 
+     die('Direct access to this location is not allowed.');
+ 
+	/** 
+	* Database Constants - these constants refer to 
+	* the database configuration settings. 
+	*/
+	 const DB_SERVER = 'localhost'; 
+	 const DB_USER = 'root'; 
+	 const DB_PASS = 'chibora53'; 
+	 const DB_DATABASE = 'sub_test';
+	 const DB_DRIVER = 'mysql';
+ 
+	 const INSTALL_KEY = 'NMpiY2fPWzZYWhV8'; 
+ 
+	/** 
+	* Show Debugger Console. 
+	* Display errors in console view. Not recommended for live site. true/false 
+	*/
+	 const DEBUG = false;
+?>
